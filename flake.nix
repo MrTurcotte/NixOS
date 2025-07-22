c797ce32648999c94a5af7c2aea84f4cb26ef4d1
MrTurcotte <d.turcotte@gmail.com>
2025-07-22 10:54:22 -0400

    testing

--- flake.nix
+++ flake.nix
@@ -2,28 +2,23 @@
   description = "Basic System Flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
 
-  outputs = { self, nixpkgs }:
-  let
-  system = "x86_64-linux";
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
     pkgs = import nixpkgs {
-      inherit system;
-      config = {
-        allowUnfree = true;
-      };
+      system = system;
+      config = { allowUnfree = true; };
     };
-  in
-  {
+  in {
     nixosConfigurations = {
       nixos = pkgs.lib.nixosSystem {
-        specialArgs = { inherit system; };
+        system = system;
         modules = [
           ./configuration.nix
         ];
       };
     };
-
   };
 }
