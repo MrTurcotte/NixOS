eda064e9421671e60b900451fcc390c4e83784ae
MrTurcotte <d.turcotte@gmail.com>
2025-08-27 14:31:13 -0400

    Update: 2025-08-27 14:31:13

--- nvidia/default.nix
+++ nvidia/default.nix
@@ -58,6 +58,14 @@
 #      settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
 #      persistencedSha256 = "sha256-2g5z7Pu8u2EiAh5givP5Q1Y4zk4Cbb06W37rf768NFU=";
 #    };
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "580.65.06";
+      openSha256 = "sha256-BKe6LQ1ZSrHUOSoV6UCksUE0+TIa0WcCHZv4lagfIgA=";
+      persistencedSha256 = "sha256-ETRfj2/kPbKYX1NzE0dGr/ulMuzbICIpceXdCRDkAxA=";
+      settingsSha256 = "sha256-9PWmj9qG/Ms8Ol5vLQD3Dlhuw4iaFtVHNC0hSyMCU24=";
+      sha256_64bit = "sha256-BLEIZ69YXnZc+/3POe1fS9ESN1vrqwFy6qGHxqpQJP8=";
+      sha256_aarch64 = "sha256-4CrNwNINSlQapQJr/dsbm0/GvGSuOwT/nLnIknAM+cQ=";
+    };
 
 
   };
