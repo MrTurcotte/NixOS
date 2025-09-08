15ef07e4bb920ab55b429471c547fbfa6a833c35
MrTurcotte <d.turcotte@gmail.com>
2025-09-07 21:59:25 -0400

    Update: 2025-09-07 21:59:25

--- flake.nix
+++ flake.nix
@@ -9,19 +9,17 @@
     system = "x86_64-linux";
     pkgs = import nixpkgs {
       system = system;
-      config = { 
-        allowUnfree = true; 
+      config = {
+        allowUnfree = true;
       };
-
-overlays = [
-    (self: super: {
-      stdenv = super.stdenv.overrideAttrs (oldAttrs: {
-        CFLAGS = oldAttrs.CFLAGS + " -O3 -march=native -mtune=native -flto -fomit-frame-pointer";
-        CXXFLAGS = oldAttrs.CXXFLAGS + " -O3 -march=native -mtune=native -flto -fomit-frame-pointer";
-      });
-    })
-  ];
-
+      overlays = [
+        (self: super: {
+          stdenv = super.stdenv.overrideAttrs (oldAttrs: {
+            CFLAGS = oldAttrs.CFLAGS + " -O3 -march=native -mtune=native -flto -fomit-frame-pointer";
+            CXXFLAGS = oldAttrs.CXXFLAGS + " -O3 -march=native -mtune=native -flto -fomit-frame-pointer";
+          });
+        })
+      ];
     };
   in {
     nixosConfigurations = {
